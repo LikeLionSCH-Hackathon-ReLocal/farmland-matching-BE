766f1aef5354f0fd0e9f3ad3f1e465626c87ee76
seokhyeon2356 <shlee3930@naver.com>
2025-08-09 20:23:50 +0900

    seller완료

---
+++ src/main/java/com/seokhyeon2356/farmlandmatchingbe/FarmlandMatchingBeApplication.java
@@ -0,0 +1,13 @@
+package com.seokhyeon2356.farmlandmatchingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmlandMatchingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmlandMatchingBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/seokhyeon2356/farmlandmatchingbe/farmlands/matchingInfo/entity/MatchingInfo.java
@@ -0,0 +1,18 @@
+package com.seokhyeon2356.farmlandmatchingbe.farmlands.matchingInfo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "machingInfo")
+public class MatchingInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long matchingId;
+}
